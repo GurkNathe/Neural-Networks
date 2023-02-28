40d884c34ed5dcaa5b61090678fe9ac7edfa2284
GurkNathe <ethan.c.krug@gmail.com>
2023-02-27 21:35:04 -0800

    Changing network.

--- Network.py
+++ Network.py
@@ -1,233 +1,95 @@
 import numpy as np
-import pandas as pd
 import random
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
+import timeit
+import csv
+from os import system, name, path
 
-class Network:
-    def __init__(
-        self, data_location: str, classes: int, layers: list, input_size: int = -1
-    ):
-        self.data = np.array(pd.read_csv(data_location))
+class Network():
+    def __init__(self, data, labels, layers: list, testing_data=None, testing_labels=None):
+        self.data = data
+        # Onehot encode the labels
+        self.labels = np.eye(10)[labels]
 
-        input_size = self.data.shape[1] - 1 if input_size == -1 else input_size
+        self.testing_data = testing_data
+        self.testing_labels = testing_labels
 
-        # Initialize variables
-        self.m, self.n = self.data.shape
+        self.layers = layers
+        self.weights = [np.array([])] * (len(self.layers) - 1)
+        self.biases = [np.array([])] * (len(self.layers) - 1)
 
-        # Initializes weights, biases, and change values for each layer
-        self.weights_bias = {
-            f"{i}": {
-                "w": np.random.rand(layers[i + 1], layers[i]) - 0.5,
-                "b": np.random.rand(layers[i + 1], 1) - 0.5,
-                "dw": 0,
-                "db": 0,
-            }
-            for i in range(len(layers))
-            if i != len(layers) - 1
-        }
+        # Initialize weights and biases
+        for i, item in enumerate(layers):
+            if i < len(layers) - 1:
+                self.weights[i] = np.random.rand(layers[i + 1], item) - 0.5
+                self.biases[i] = np.random.rand(layers[i + 1], 1) - 0.5
 
-        self.layers = len(layers) - 1
+    def gradient_descent(self, epochs: int=3, learning_rate:int=0.1):
+        correct = 0
+        for epoch in range(epochs):
+            for image, label in zip(self.data, self.labels):
+                image.shape += (1,)
+                label.shape += (1,)
+                
+                # Forward propagation
+                node_values = self.forward_prop(image, label)
+                
+                correct += int(np.argmax(node_values[len(node_values) - 1]) == np.argmax(label))
+                
+                self.backward_prop(node_values, label, learning_rate)
+            # Show accuracy for this epoch
+            print(f"Epoch: {epoch}")
+            print(f"Accuracy: {round((correct / self.data.shape[0]) * 100, 2)}%")
+            correct = 0
 
-        self.W1 = np.random.rand(classes, input_size) - 0.5
-        self.b1 = np.random.rand(classes, 1) - 0.5
-        self.W2 = np.random.rand(classes, classes) - 0.5
-        self.b2 = np.random.rand(classes, 1) - 0.5
-        self.dW1 = 0
-        self.db1 = 0
-        self.dW2 = 0
-        self.db2 = 0
+    def forward_prop(self, image, label):
+        values = [None] * (len(self.layers) - 1)
+        values[0] = self.biases[0] + self.weights[0] @ np.array(image)
+        values[0] = 1 / (1 + np.exp(-values[0]))
 
-        np.random.shuffle(self.data)
+        for i in range(1, len(self.layers) - 1):
+            values[i] = self.biases[i] + self.weights[i] @ np.array(values[i - 1])
+            values[i] = 1 / (1 + np.exp(-values[i]))
 
-        # Initialize training and testing data
-        self.train_size = int(0.8 * self.m)
-        self.test_size = int(0.2 * self.m)
+        return values
 
-        self.train_data = self.data[self.test_size : self.m].T
-        self.test_data = self.data[0 : self.test_size].T
+    def backward_prop(self, values, label, learn_rate):
+        delta = values[len(values) - 1] - label
+        for i in range(len(self.weights) - 1, 0, -1):
+            self.weights[i] += -learn_rate * delta @ values[i - 1].T
+            self.biases[i] += -learn_rate * delta
+            delta = self.weights[i].T @ delta * (values[i - 1] * (1 - values[i - 1]))
 
-        self.train_labels = self.train_data[0]
-        self.train_items = self.train_data[1 : self.n]
-        self.train_items = self.train_items / 255
+##########WIP###################
+    def save_model(self):
+        with open(
+            path.join(
+                path.dirname(__file__),
+                f"models/model{int(timeit.default_timer())}.csv",
+            ),
+            "w",
+            newline="",
+        ) as myfile:
+            wr = csv.writer(myfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+            for layer in self.weights:
+                wr.writerow(layer)
+            for layer in self.biases:
+                wr.writerow(layer)
+            wr.writerow(self.layers)
 
-        self.test_labels = self.test_data[0]
-        self.test_items = self.test_data[1 : self.n]
-        self.test_items = self.test_items / 255
+    def load_model(self, file_path: str):
+        with open(file_path, "r") as model_data:
+            model = csv.reader(model_data)
+            print(model)
+#########WIP####################
 
-    def relu(self, Z):
-        return np.maximum(Z, 0)
-
-    def relu_deriv(self, Z):
-        return Z > 0
-
-    def softmax(self, Z):
-        A = np.exp(Z) / sum(np.exp(Z))
-        return A
-
-    def one_hot(self, Y):
-        one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-        one_hot_Y[np.arange(Y.size), Y] = 1
-        one_hot_Y = one_hot_Y.T
-        return one_hot_Y
-
-    ### 2-layer neural network ###
-    def forward_prop(self):
-        Z1 = self.W1.dot(self.train_items) + self.b1
-        A1 = self.relu(Z1)
-        Z2 = self.W2.dot(A1) + self.b2
-        A2 = self.softmax(Z2)
-        return Z1, A1, Z2, A2
-
-    def backward_prop(self, Z1, A1, Z2, A2):
-        one_hot_Y = self.one_hot(self.train_labels)
-        dZ2 = A2 - one_hot_Y
-        self.dW2 = 1 / self.m * dZ2.dot(A1.T)
-        self.db2 = 1 / self.m * np.sum(dZ2)
-        dZ1 = self.W2.T.dot(dZ2) * self.relu_deriv(Z1)
-        self.dW1 = 1 / self.m * dZ1.dot(self.train_items.T)
-        self.db1 = 1 / self.m * np.sum(dZ1)
-
-    def update_params(self, alpha):
-        self.W1 = self.W1 - alpha * self.dW1
-        self.b1 = self.b1 - alpha * self.db1
-        self.W2 = self.W2 - alpha * self.dW2
-        self.b2 = self.b2 - alpha * self.db2
-
-    def gradient_descent(self, alpha=0.1, iterations=500):
-        for i in range(iterations + 1):
-            Z1, A1, Z2, A2 = self.forward_prop()
-            self.backward_prop(Z1, A1, Z2, A2)
-            self.update_params(alpha)
-            if i % 10 == 0:
-                print("Iteration: ", i)
-                predictions = np.argmax(A2, 0)
-                print(
-                    "Accuracy: ",
-                    round(
-                        (
-                            np.sum(predictions == self.train_labels)
-                            / self.train_labels.size
-                        )
-                        * 100,
-                        3,
-                    ),
-                )
-
-    def make_predictions(self, X):
-        Z1 = self.W1.dot(X) + self.b1
-        A1 = self.relu(Z1)
-        Z2 = self.W2.dot(A1) + self.b2
-        A2 = self.softmax(Z2)
-        predictions = np.argmax(A2, 0)
-        return predictions
-
-    def test_prediction(self, index=-1):
-        index = random.randint(0, self.train_size) if index == -1 else index
-        current_image = self.train_items[:, index, None]
-        prediction = self.make_predictions(self.train_items[:, index, None])
-        label = self.train_labels[index]
-        print("Prediction: ", prediction)
-        print("Label: ", label)
-
-        current_image = current_image.reshape((28, 28)) * 255
-        plt.gray()
-        plt.imshow(current_image, interpolation="nearest")
+    def predict(self, index: int=-1):
+        index = random.randint(0, self.data.shape[0]) if index == -1 else index
+        image = self.data[index]
+        plt.imshow(image.reshape(28, 28), cmap="Greys")
+        
+        image.shape += (1,)
+        values = self.forward_prop(image, self.labels[index])
+        plt.title(values[len(values) - 1].argmax())
         plt.show()
-    ### 2-layer neural network ###
-
-    ### Generalized Neural Network ####
-    def forward_prop_g(self):
-        Z = [None for _ in range(self.layers)]
-        A = [None for _ in range(self.layers)]
-        for tag in self.weights_bias:
-            weights = self.weights_bias[tag]["w"]
-            biases = self.weights_bias[tag]["b"]
-
-            # First layer is dotted with training data
-            if tag == "0":
-                Z[0] = weights.dot(self.train_items) + biases
-            # Every other layer is dotted with activated data
-            else:
-                Z[int(tag)] = weights.dot(A[int(tag) - 1]) + biases
-
-            # Last layer gets softmax function
-            if tag == str(self.layers):
-                A[self.layers - 1] = self.softmax(Z[int(tag)])
-            # Every other layer gets ReLU function
-            else:
-                A[int(tag)] = self.relu(Z[int(tag)])
-        return Z, A
-
-    def backward_prop_g(self, Z, A):
-        one_hot_y = self.one_hot(self.train_labels)
-        dZ = [None for _ in range(self.layers)]
-
-        for i in range(self.layers - 1, -1, -1):
-            # Last layer one hot
-            if i == self.layers - 1:
-                dZ[i] = A[i] - one_hot_y
-            else:
-                dZ[i] = self.weights_bias[str(i + 1)]["w"].T.dot(
-                    dZ[i + 1]
-                ) * self.relu_deriv(Z[i])
-
-            # First layer use training
-            if i == 0:
-                self.weights_bias["0"]["dw"] = (
-                    1 / self.m * dZ[0].dot(self.train_items.T)
-                )
-            else:
-                self.weights_bias[str(i)]["dw"] = (
-                    1 / self.m * dZ[i].dot(A[i].T)
-                )
-
-            self.weights_bias[str(i)]["db"] = 1 / self.m * np.sum(dZ[i])
-
-    def update_params_g(self, alpha):
-        for tag in self.weights_bias:
-            self.weights_bias[tag]["w"] = (
-                self.weights_bias[tag]["w"] - alpha * self.weights_bias[tag]["dw"]
-            )
-            self.weights_bias[tag]["b"] = (
-                self.weights_bias[tag]["b"] - alpha * self.weights_bias[tag]["db"]
-            )
-
-    def gradient_descent_g(self, alpha=0.1, iterations=500):
-        for i in range(iterations + 1):
-            Z, A = self.forward_prop_g()
-            self.backward_prop_g(Z, A)
-            self.update_params_g(alpha)
-            if i % 10 == 0:
-                print("Iteration: ", i)
-                predictions = np.argmax(A[self.layers - 1], 0)
-                print(
-                    "Accuracy: ",
-                    round(
-                        (
-                            np.sum(predictions == self.train_labels)
-                            / self.train_labels.size
-                        )
-                        * 100,
-                        3,
-                    ),
-                )
-
-    def make_predictions_g(self):
-        _, A = self.forward_prop_g()
-        predictions = np.argmax(A[self.layers - 1], 0)
-        return predictions
-
-    def test_prediction_g(self, index=-1):
-        index = random.randint(0, self.train_size) if index == -1 else index
-        current_image = self.train_items[:, index, None]
-        prediction = self.make_predictions(self.train_items[:, index, None])
-        label = self.train_labels[index]
-        print("Prediction: ", prediction)
-        print("Label: ", label)
-
-        current_image = current_image.reshape((28, 28)) * 255
-        plt.gray()
-        plt.imshow(current_image, interpolation="nearest")
-        plt.show()
-    ### Generalized Neural Network ####
--- README.md
+++ README.md
@@ -1,35 +1,33 @@
 # Neural-Networks ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
-This is based off of the presentation by Samson Zhang. You can find the code [here](https://www.kaggle.com/code/wwsalmon/simple-mnist-nn-from-scratch-numpy-no-tf-keras) and the video [here](https://www.youtube.com/watch?v=w8yWXqWQYmU).
+This is based off of the presentation by Bot Academy. You can find the code [here](https://github.com/Bot-Academy/NeuralNetworkFromScratch/blob/master/nn.py) and the video [here](https://youtu.be/9RN2Wr8xvro).
 
 This is an educational project that I am working on, while learning about Neural Networks and machine learning.
 
-Currently there is a class called Network implemented in the Network.py file. The fully implemented version is a two-layer neural network implemented using the forward propagation, backward propagation method. 
-
-There is a generalized neural network that is being worked on that is meant to allow any number of layers with any number of nodes. However, as of updating this, it is not complete for all cases, and has some issues. I've tested it with a two-layer neural network, similar to the hard-coded one, and it works approximately as well as the hard-coded one. You can add hidden layers, however they currently have to be the same size as the output layer, and they can be unstable in accuracy, to say the least.
+Currently there is a class called Network implemented in the Network.py file. The version implemented in the video by Bot Academy was a hard-coded neural network with a hidden layer of 20 nodes, input layer of 784 nodes, and output layer of 10 nodes. The network I have implemented is a generic network, where any layers sizes are accepted, and any number of layers are allowed (will change in the future to avoid errors).
 
 My intent with this project is to create an object oriented neural network that is essentially plug-and-chug.
 
 ## Examples:
 
-To use the neural network in its current form, you need a file with data, the number of classes/outputs from the network, and the layer structure.
+To use the neural network in its current form, you need training data, labels for the training data, and a list of the layers' node counts.
 
 ```
-network = Network("data/train.csv", classes=10, layers=[784, 10])
+network = Network(data, labels, [784, 100, 10])
 ```
 
-The gradient descent function is then called on the network to train it. This takes in two optional arguments: ```alpha``` (training rate), ```iterations``` (number of iterations). The default value for ```alpha``` is 0.1 and the default value for ```iterations``` is 500.
+The gradient descent function is then called on the network to train it. This takes in two optional arguments: ```epochs``` (number of iterations), ```learning_rate``` (scaling factor while training). The default value for ```epochs``` is 3 and the default value for ```learning_rate``` is 0.1.
 
 ```
-# Hard-coded version
-network.gradient_descent(alpha=0.1, iterations=500)
-
-# Generalized version
-network.gradient_descent_g()
+network.gradient_descent()
 ```
+
+## Save & Loading
 
 Currently, there is no output for the trained network, so it can either be used in a Jypiter notebook, or be trained every run when ran from the command line.
 
+I am working on a way to save the model and load it, however it is still a work in progress, and doesn't work.
+
 ## Testing
 
-To test the network, there are two functions implemented: ```test_prediction() & test_prediction_g()```. Each takes an optional argument ```index``` for the data point from the dataset to be tested against. If no index is given, a random datum is chosen to be tested against.
+To test the network, there is a prediction function implemented: ```predict()```. It takes an optional argument ```index``` for the data point from the dataset to be tested against. If no index is given, a random datum is chosen to be tested against.
